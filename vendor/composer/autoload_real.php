c5abcf3eb541bf29e6a448db1ce8982447dcd3cc
Sandip Mandal <sandiptaibichara@gmail.com>
2025-12-05 17:50:43 +0530

    Refactor code structure for improved readability and maintainability

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit49f06bbce9cc2549494e496b6b0ef296
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit49f06bbce9cc2549494e496b6b0ef296::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
